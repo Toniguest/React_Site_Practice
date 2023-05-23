4a8f0fed6d0d4029e711aa48f6404f12569d3024
Antonio Pirone <combo2100@gmail.com>
2023-05-23 10:49:07 +0200

    Aggiunto route con 3 pagine

--- typescript_sitereact/src/App.tsx
+++ typescript_sitereact/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import NavBar from './components/Navbar';
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ciao
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
-}
-
+};
 export default App;
